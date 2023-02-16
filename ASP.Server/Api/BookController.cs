87a5312429ec107e61849636589a6e2a3e3edb91
safouane159 <57716423+safouane159@users.noreply.github.com>
2023-02-16 14:44:57 +0100

    Add method GetBooksByGenre for REST Api

--- ASP.Server/Api/BookController.cs
+++ ASP.Server/Api/BookController.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ASP.Server.Database;
+using System.Reflection.Emit;
+using System.Xml.Linq;
 
 namespace ASP.Server.Api
 {
@@ -21,38 +23,6 @@ namespace ASP.Server.Api
         {
             this.libraryDbContext = libraryDbContext;
         }
-
-        // Methode a ajouter : 
-        // - GetBooks
-        //   - Entrée: Optionel -> Liste d'Id de genre, limit -> defaut à 10, offset -> défaut à 0
-        //     Le but de limit et offset est dé créer un pagination pour ne pas retourner la BDD en entier a chaque appel
-        //   - Sortie: Liste d'object contenant uniquement: Auteur, Genres, Titre, Id, Prix
-        //     la liste restourner doit être compsé des élément entre <offset> et <offset + limit>-
-        //     Dans [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] si offset=8 et limit=5, les élément retourner seront : 8, 9, 10, 11, 12
-
-        // - GetBook
-        //   - Entrée: Id du livre
-        //   - Sortie: Object livre entier
-
-        // - GetGenres
-        //   - Entrée: Rien
-        //   - Sortie: Liste des genres
-
-        // Aide:
-        // Pour récupéré un objet d'une table :
-        //   - libraryDbContext.MyObjectCollection.<Selecteurs>.First()
-        // Pour récupéré des objets d'une table :
-        //   - libraryDbContext.MyObjectCollection.<Selecteurs>.ToList()
-        // Pour faire une requète avec filtre:
-        //   - libraryDbContext.MyObjectCollection.<Selecteurs>.Skip().<Selecteurs>
-        //   - libraryDbContext.MyObjectCollection.<Selecteurs>.Take().<Selecteurs>
-        //   - libraryDbContext.MyObjectCollection.<Selecteurs>.Where(x => x == y).<Selecteurs>
-        // Pour récupérer une 2nd table depuis la base:
-        //   - .Include(x => x.yyyyy)
-        //     ou yyyyy est la propriété liant a une autre table a récupéré
-        //
-        // Exemple:
-        //   - Ex: libraryDbContext.MyObjectCollection.Include(x => x.yyyyy).Where(x => x.yyyyyy.Contains(z)).Skip(i).Take(j).ToList()
 
 
         [HttpGet]
@@ -69,7 +39,38 @@ namespace ASP.Server.Api
             var bookDTO = books.Select(b => b.ToBookDTO());
 
             return Ok(bookDTO);
-            
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<BookDTO>> GetBooksByGenre(int limit = 10, int offset = 0, string label = null)
+        {
+            if (string.IsNullOrWhiteSpace(label) ||  string.IsNullOrEmpty(label))
+            {
+                return BadRequest("Genre label cannot be empty.");
+                throw new ArgumentException($"'{nameof(label)}' cannot be null or whitespace.", nameof(label));
+                   
+            }else {
+                var books = libraryDbContext.Books
+               .Include(b => b.Genres)
+               .Where(b => b.Genres.Any(g => g.Label == label))
+               .OrderBy(b => b.Id)
+               .Skip(offset)
+               .Take(limit)
+               .ToList();
+
+                if (books == null || books.Count == 0)
+                {
+                    return NotFound($"No book found of genre :'{label}' ! try another Genre. ");
+                }
+
+                var bookDTO = books.Select(b => b.ToBookDTO());
+
+                return Ok(bookDTO);
+            }
+
+           
+
         }
 
         [HttpGet("{id}")]
@@ -85,15 +86,6 @@ namespace ASP.Server.Api
             return Ok(book);
         }
 
-
-        [HttpGet]
-        public ActionResult<Genre> GetGenres()
-        {
-            List<Genre> genres = libraryDbContext.Genres.ToList();
-
-          
-            return Ok(genres);
-        }
 
 
     }
