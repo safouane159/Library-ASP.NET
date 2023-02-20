2ccecbb382dfc40e19b01b8cd40f143a30378763
SAMIA Oussama <oussamasamia1@gmail.com>
2023-02-20 23:01:22 +0100

    Dashborad page

--- ASP.Server/Controllers/HomeController.cs
+++ ASP.Server/Controllers/HomeController.cs
@@ -6,20 +6,35 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.Server.Service;
 
 namespace ASP.Server.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private BookService bookService;
+        private GenreService genreService;
+        private AuteurService auteurService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BookService bookService, GenreService genreService, AuteurService auteurService)
         {
             _logger = logger;
+            this.genreService = genreService;
+            this.bookService = bookService;
+            this.auteurService = auteurService;
         }
 
         public IActionResult Index()
         {
+
+            ViewBag.TotalBooks = bookService.GetTotalBooks();
+            ViewBag.TotalAuthors = auteurService.GetAllAuteurs().Count;
+            ViewBag.TotalCategories = genreService.GetGenres().Count;
+            ViewBag.TotalProfit = bookService.GetBooks().Sum(book => book.Prix);
+
+
+
             return View();
         }
 
